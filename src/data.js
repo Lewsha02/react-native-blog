da326fdb0447a8dacbd7ee89f8fdc838b9f6f1a8
Егор <en@welcomemedia.ru>
2021-07-07 16:13:08 +0300

    Add post component

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const DATA = [
+  {
+    id: "1",
+    img: "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1413&q=80",
+    text: "Awesome text for post 1",
+    date: new Date().toJSON(),
+    booked: true,
+  },
+  {
+    id: "2",
+    img: "https://images.unsplash.com/photo-1444723121867-7a241cacace9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    text: "Awesome text for post 2",
+    date: new Date().toJSON(),
+    booked: true,
+  },
+  {
+    id: "3",
+    img: "https://images.unsplash.com/photo-1556610961-2fecc5927173?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2120&q=80",
+    text: "Awesome text for post 3",
+    date: new Date().toJSON(),
+    booked: false,
+  },
+  {
+    id: "4",
+    img: "https://images.unsplash.com/photo-1610197361600-33a3a5073cad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    text: "Awesome text for post 4",
+    date: new Date().toJSON(),
+    booked: false,
+  },
+  {
+    id: "5",
+    img: "https://images.unsplash.com/photo-1602837371127-939f681acc00?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    text: "Awesome text for post 5",
+    date: new Date().toJSON(),
+    booked: false,
+  },
+];
