5f5ab27eb2fea101465ee37fd02453a5787d7c89
Егор <en@welcomemedia.ru>
2021-07-07 18:15:15 +0300

    Add booked star to post screen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -7,7 +7,7 @@ import { AppHeaderIcon } from "../components/AppHeaderIcon";
 
 export const MainScreen = ({ navigation }) => {
   const openPostHandler = (post) => {
-    navigation.navigate("Post", { postId: post.id, date: post.date });
+    navigation.navigate("Post", { postId: post.id, date: post.date, booked: post.booked });
   };
 
   return (
@@ -28,6 +28,15 @@ MainScreen.navigationOptions = {
       <Item
         title='Take photo'
         iconName='ios-camera'
+        onPress={() => console.log("Press photo")}
+      />
+    </HeaderButtons>
+  ),
+  headerLeft: (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Toggle drawer'
+        iconName='ios-menu'
         onPress={() => console.log("Press photo")}
       />
     </HeaderButtons>
