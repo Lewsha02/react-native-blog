33f9b4183b3a01f3a391c685d58aabc09c13e5f8
Егор <en@welcomemedia.ru>
2021-07-08 15:28:05 +0300

    Getting data from db & add middleware redux-thunk

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,9 +1,14 @@
+import { DB } from "../../db";
 import { ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from "../types";
 
 export const loadPosts = () => {
-  return {
-    type: LOAD_POSTS,
-    payload: [],
+  return async (dispatch) => {
+    const posts = await DB.getPosts();
+
+    dispatch({
+      type: LOAD_POSTS,
+      payload: posts,
+    });
   };
 };
 
