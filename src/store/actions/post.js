8f71c0e9b17320af0dd622d5cbcd9eb615aa7cb1
Егор <en@welcomemedia.ru>
2021-07-08 15:41:32 +0300

    Add functionality for creating new post in db

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,3 +1,4 @@
+import * as FileSystem from "expo-file-system";
 import { DB } from "../../db";
 import { ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from "../types";
 
@@ -26,11 +27,26 @@ export const removePost = (id) => {
   };
 };
 
-export const addPost = (post) => {
-  post.id = Date.now().toString();
+export const addPost = (post) => async (dispatch) => {
+  const fileName = post.img.split("/").pop();
+  const newPath = FileSystem.documentDirectory + fileName;
 
-  return {
+  try {
+    await FileSystem.moveAsync({
+      to: newPath,
+      from: post.img,
+    });
+  } catch (e) {
+    console.log("Error: ", e);
+  }
+
+  const payload = { ...post, img: newPath };
+  const id = await DB.createPost(payload);
+
+  payload.id = id;
+
+  dispatch({
     type: ADD_POST,
-    payload: post,
-  };
+    payload,
+  });
 };
